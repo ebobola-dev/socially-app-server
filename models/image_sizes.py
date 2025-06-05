da7965a928ad7e0d60232d46e9a1315994c38a87
Roma Blagov <romawot94@gmail.com>
2025-06-05 22:47:01 +0300

    feat(media/images): changed image splitting sizes to [256, 512, 1024, original]

--- models/image_sizes.py
+++ models/image_sizes.py
@@ -4,15 +4,17 @@ from aiohttp.web import Request
 
 
 class ImageSizes(Enum):
-    s_128 = 128
-    s_512 = 512
-    s_original = 3
+    s_256 = 256         #% for small avatars
+    s_512 = 512         #% for ~1/3 mobile screen
+    s_1024 = 1024       #% for full [width] of mobile screen
+    s_original = 4
 
     @property
     def str_view(self):
         return {
-            ImageSizes.s_128: "128",
+            ImageSizes.s_256: "256",
             ImageSizes.s_512: "512",
+            ImageSizes.s_1024: "1024",
             ImageSizes.s_original: "original",
         }[self]
 
@@ -20,16 +22,18 @@ class ImageSizes(Enum):
     def from_request(cls, request: Request) -> "ImageSizes":
         str_size = request.query.get("size", "")
         match str_size:
-            case "128":
-                return cls.s_128
+            case "256":
+                return cls.s_256
             case "512":
                 return cls.s_512
+            case "1024":
+                return cls.s_1024
             case _:
                 return cls.s_original
 
     @classmethod
     def all_sizes_ordered(cls):
-        return [cls.s_128, cls.s_512, cls.s_original]
+        return [cls.s_256, cls.s_512, cls.s_1024, cls.s_original]
 
     @classmethod
     def get_next_available_size(cls, requested: "ImageSizes") -> list["ImageSizes"]:
